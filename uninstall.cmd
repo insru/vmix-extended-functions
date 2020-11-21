a69926290af40915553f1564cee6fe9be2396d55
nav <artem.nikonov@jugru.org>
2020-11-22 00:48:46 +0300

    fix: uninstall.cmd

--- uninstall.cmd
+++ uninstall.cmd
@@ -1,1 +1,1 @@
-pm2 delete vmix-extended-functions-master & pm2 save & rmdir C:\vmix-extended-functions /s /q & PAUSE
+pm2 delete vmix-extended-functions & pm2 save & rmdir C:\vmix-extended-functions-master /s /q & PAUSE
