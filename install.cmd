e4a581c41582aa2aefaa04d3da75cd40e53a64ee
nav <artem.nikonov@jugru.org>
2020-11-22 00:45:55 +0300

    fix: install.cmd

--- install.cmd
+++ install.cmd
@@ -1,1 +1,1 @@
-chdir C:\vmix-extended-functions-master & npm i & npm i pm2 -g & npm i typescript & -g npm i pm2-windows-startup -g & pm2-startup install & tsc .\src\vmix-extended-functions.ts & pm2 start .\src\vmix-extended-functions.js & pm2 save & PAUSE
+chdir C:\vmix-extended-functions-master & npm i & npm i pm2 -g & npm i typescript -g & npm i pm2-windows-startup -g & pm2-startup install & tsc .\src\vmix-extended-functions.ts & pm2 start .\src\vmix-extended-functions.js & pm2 save & PAUSE
